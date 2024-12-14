9087557c47bf41b2248089daddd744d43d3bfcdb
Dhairya Shah <hello@dhairyashah.dev>
2024-12-14 23:15:07 +0530

    POST Request in routing

--- routing/src/routes/mod.rs
+++ routing/src/routes/mod.rs
@@ -1,9 +1,12 @@
 mod hello_world;
+mod mirror_body_string;
 
 use axum::Router;
-use axum::routing::patch;
+use axum::routing::{patch, post};
 use hello_world::hello_world;
+use mirror_body_string::mirror_body_string;
 
 pub fn create_routes() -> Router<> {
     Router::new().route("/", patch(hello_world))
+    .route("/mirror_body_string", post(mirror_body_string))
 }
